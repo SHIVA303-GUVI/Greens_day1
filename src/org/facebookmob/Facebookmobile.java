f577a389c0eb1dca38eff8508aba46f9d0db562e
SHIVA303-GUVI <shivakamaraj1997@gmail.com>
2022-01-01 10:26:36 +0530

    Password Funcationality

--- src/org/facebookmob/Facebookmobile.java
+++ src/org/facebookmob/Facebookmobile.java
@@ -1,5 +1,13 @@
 package org.facebookmob;
 
 public class Facebookmobile {
+	
+    	public static void password() {
+		        System.out.println("password");
 
+	    }
+    	public static void main(String[] args) {
+			password();
+		}
+    	
 }
